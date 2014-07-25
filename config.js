69558c3f61dc34c6f1ccf1a5f7ee372adeaec14b
evilqubit <simonstadros@gmail.com>
2014-07-26 01:12:20 +0300

    config added

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	ntwitter:{
+		consumer_key: '9P4YfAkGLICbxmPRm4Yzj2kPS',
+		consumer_secret: '6DWdDjOaYN2JdOf6qQLzUEQHw3Nhs8GABc0vYq3yvYuVpCthUZ',
+		access_token_key: '77060821-GNv6F5pcA8zE1H1BOAR9BtHAEdyUOjLZvH5MtcifZ',
+		access_token_secret: '9hNKQLWsrbBu0zvXwV64uCbjsKI0eHn1yLpKKtw6ME40h'
+	}
+}
